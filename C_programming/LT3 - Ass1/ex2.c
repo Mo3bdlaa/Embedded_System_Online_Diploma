c4c0988deb535150358e54f296ca40b93b42b6c5
Mohammed Abdullah <3bdlaa.20@gmail.com>
2020-08-28 11:22:10 +0200

    C_Programming - Ass1

---
+++ C_programming/LT3 - Ass1/ex2.c
@@ -0,0 +1,20 @@
+/*
+ * ex2.c
+ *
+ *  Created on: Aug 28, 2020
+ *      Author: Mohammed Abdullah
+ *      Github: @Mo3bdlaa
+ */
+
+
+#include<stdio.h>  //importing stdio library
+
+void main(){
+
+	int x;       //Declaring x variable as integer
+
+	printf("Please enter an integer");
+    scanf("%d",&x);   //Scan user entered data
+    printf("The integer your entered is %d",x);
+}
+
---
+++ C_programming/LT3-Ass1/ex1.c
@@ -0,0 +1,17 @@
+/*
+ * ex1.c
+ *
+ *  Created on: Aug 28, 2020
+ *      Author: Mohammed Abdullah
+ *      Github: @Mo3bdlaa
+ */
+
+
+#include<stdio.h>  //importing stdio library
+
+void main(){
+
+Printf("First exercise of C_programming in Embedded System online diploma");
+
+}
+
